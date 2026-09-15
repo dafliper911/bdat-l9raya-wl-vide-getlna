cbaeacce671b82a81d44cc9b6dede3d5b0f44e82
dafliper911 <youneskazed750@gmail.com>
2026-09-15 22:22:51 +0100

    2.0.0

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-version = "1.0.0" #  MAJOR.MINOR.PATCH
+# Git: status → add → commit → push
+import platform
+import psutil
+import subprocess
 
-print(f"you are running version {version}")
+
+version = "2.0.0" #  MAJOR.MINOR.PATCH
+ram = psutil.virtual_memory().total
+wos = platform.system() + " " +platform.release()
+
+
+
+
+cpu = subprocess.check_output(
+    "wmic cpu get name",
+    shell=True
+).decode().split("\n")[1].strip()
+
+print(cpu)
+print(__import__("subprocess").check_output(["powershell", "-NoProfile", "-Command", "(Get-CimInstance Win32_VideoController).Name"]).decode().strip())
+print(f"{ram / (1024 ** 3):.1f} GB")
+print(wos)
